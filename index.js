8f4a54027d042656c6a1d4ffdceee226624b41e8
KenethSandoval <kenethra.74@gmail.com>
2021-03-17 12:51:02 -0600

    feat[image]: get image for fetch

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 let url = "https://restcountries.eu/rest/v2" 
 let countries = document.getElementById("countries");
 
-
 //TODO: hacer paginacion
 const getCountries = async () => {
   let req = await fetch(`${url}/all`)
   let res = await req.json();
 
-  //FIX: a foreach
+  //FIX: a foreach luego de hacer la paginacion
   for (let i = 0; i <= 4; ++i) {
-    fetch(`${res[i].flag}`)
     countries.innerHTML += `
-      <p>${res[i].name}</p>
-      <p>${res[i].population}</p>
-      <p>${res[i].region}</p>
-      <p>${res[i].capital}</p> 
-      
-      <br><br>
+      <div class="card">
+        <img src=${res[i].flag} alt="logo"/>
+        <p>${res[i].name}</p>
+        <p>${res[i].population}</p>
+        <p>${res[i].region}</p>
+        <p>${res[i].capital}</p> 
+      </div
       `
   }
 }

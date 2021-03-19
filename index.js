d4ec158306fe789e06b99253a163d44b8b1b654e
KenethSandoval <kenethra.74@gmail.com>
2021-03-18 23:41:39 -0600

    feat[desing]: toogle dark mode

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-let url = "https://restcountries.eu/rest/v2" 
 let countries = document.getElementById("countries");
+let buttonDark = document.querySelector(".dark_mode");
+
+class AllRequest {
+  async withCountries(url) {
+    let reqFetch = await fetch(url);
+    let resFetch = await reqFetch.json();
+    return resFetch;
+  }
+}
 
 //TODO: hacer paginacion
 const getCountries = async () => {
-  let req = await fetch(`${url}/all`)
-  let res = await req.json();
-
+  //instancia de la clase que realiza las peticiones
+  let r = new AllRequest();
+  let url = "https://restcountries.eu/rest/v2" 
+  //respuesta de la api
+  let res = await r.withCountries(`${url}/all`);
+  
   //FIX: a foreach luego de hacer la paginacion
-  for (let i = 0; i <= 4; ++i) {
+  for (let i = 0; i <= 7; ++i) {
     countries.innerHTML += `
       <div class="card">
         <img src=${res[i].flag} alt="logo"/>
-        <p>${res[i].name}</p>
-        <p>${res[i].population}</p>
-        <p>${res[i].region}</p>
-        <p>${res[i].capital}</p> 
+        <p class="title">${res[i].name}</p>
+        <p><b>Population: </b>${res[i].population}</p>
+        <p><b>Region: </b>${res[i].region}</p>
+        <p><b>Capital: </b>${res[i].capital}</p> 
       </div
       `
   }
 }
 
-getCountries();
+window.addEventListener("load", getCountries);
+
+
+//TODO: detaller para el dark mode
+buttonDark.addEventListener("click", () => {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+});
+
+
